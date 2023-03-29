79b25cc82f624edca99a843ed38cc9a4ae778d3f
Paul <pajmurray@outlook.com>
2023-03-29 21:29:04 +0100

    add openai and azure function to promptinput component

--- aiimagegenerator/app/api/suggestion/route.ts
+++ aiimagegenerator/app/api/suggestion/route.ts
@@ -1,6 +1,8 @@
 export async function GET(request: Request) {
   // Connect to Microsoft Azure Endpoint
-  const response = await fetch("...", { cache: "no-store" });
+
+  // Add logic for local host for dev, hosted route for production
+  const response = await fetch("http://localhost:7071/api/getChatGPTSuggestion", { cache: "no-store" });
 
   const textData = await response.text();
 
--- aiimagegenerator/azure/src/functions/getChatGPTSuggestion.js
+++ aiimagegenerator/azure/src/functions/getChatGPTSuggestion.js
@@ -1,13 +1,21 @@
 const { app } = require('@azure/functions');
+const openai = require("../../lib/openai");
 
 app.http('getChatGPTSuggestion', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: 'Write a random prompt for an AI image generator',
+            max_tokens: 100,
+            temperature: 0.8,
+        });
+        
+        context.log(`Heyssssscs ffHttp function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const responseText = response.data.choices[0].text;
 
-        return { body: `Hello, ${name}!` };
+        return { body: responseText };
     }
 });
